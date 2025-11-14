35caa842f642941437fcf7763678e9401755e067
sofialule98-hue <202510059091@alunos.estacio.br>
2025-11-14 18:23:50 -0300

    Add files via upload

---
+++ Ex08.py
@@ -0,0 +1,8 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+m = float(input("Digite um valor em metros: "))
+
+cm = m * 100
+mm = m * 1000
+
+print(f"O valor de {m} metros convertido em centímetros é igual a {cm} cm e em milímetros é igual a {mm} mm.")
