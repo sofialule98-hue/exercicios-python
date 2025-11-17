4fccc03a4cb2e5e8239127c4d5f070f29558fd70
Sofia <202510059091@alunos.estacio.br>
2025-11-17 18:35:47 -0300

    Add files via upload

---
+++ Ex021.py
@@ -0,0 +1,9 @@
+# Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+import pygame
+
+pygame.init()
+pygame.mixer.music.load('Amy winehouse valerie[Mp3xd.tv].mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
