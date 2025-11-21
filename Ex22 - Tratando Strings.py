aa9fd4c4600a943101a13eea961d69014d4048a5
Sofia <202510059091@alunos.estacio.br>
2025-11-21 20:11:43 -0300

    Refactor string handling program for clarity

--- Ex22 - Tratando Strings.py
+++ Ex22 - Tratando Strings.py
@@ -7,7 +7,9 @@ nome = input("Digite seu nome completo: ").strip()
 
 print(f"Maiúsculo: {nome.upper()}")
 print(f"Minúsculo: {nome.lower()}")
-print(f"Seu nome possui ao todo {len(nome) - nome.count(' ')} letras.")
+print(f"Seu nome possui ao todo {len(nome) - nome.count(' ')} letras.") # ao contar quantos caracteres possui uma string e contar quantos espaços é possível realizar uma subtração para verificar quantos caracteres possui
+# um objeto específico
 
 separa = nome.split()
 print(f"Seu primeiro nome é {separa[0].capitalize()} e possui {len(separa[0])} letras.")
+
