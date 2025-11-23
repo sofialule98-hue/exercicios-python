5312c3c71bf07a4068276cefeef75962bc14c7f6
Sofia <202510059091@alunos.estacio.br>
2025-11-23 20:46:39 -0300

    Update and rename Ex030.py to Ex30 - Par ou Ímpar.py

--- Ex30 - Par ou Ímpar.py
+++ Ex30 - Par ou Ímpar.py
@@ -2,7 +2,8 @@
 
 numero = int(input("Digite um número inteiro: "))
 
-if numero % 2 == 0:
+if numero % 2 == 0: # a condição verifica se o resto do número informado divido por 2  é igual a zero, determinando assim se o número é par ou ímpar
     print(f"O número que você digitou foi {numero}, ele é PAR!")
 else:
     print(f"O número que você digitou foi {numero}, ele é IMPAR!")
+
