1caa29d7306629e267be64ad40e4befffaba44cf
Sofia <202510059091@alunos.estacio.br>
2025-11-29 19:32:41 -0300

    Update and rename Ex062.py to Ex62 - Progressão Aritmética 3.0.py

--- Ex62 - Progressão Aritmética 3.0.py
+++ Ex62 - Progressão Aritmética 3.0.py
@@ -13,12 +13,13 @@ termo = primeiro
 cont = 1
 total = 0
 mais = 10
-while mais != 0:
-    total += mais
-    while cont <= total:
+while mais != 0: # enquanto o usuário não quiser ver mais termos e teclar 0, o loop irá continuar
+    total += mais # esta somatória permite que de início a PA tenha 10 termos e em sequência irá somar a quantidade de termos solicitados
+    while cont <= total: # enquanto a contagem for menor ou igual ao total, o programa irá continuar realizando a somatória de mais termos
          print(f"{termo} -> ", end='')
          termo += razao
          cont += 1
     print("PAUSA")
     mais = int(input("Quantos termos você quer mostrar a mais? "))
 print(f"Progressão finalizada com {total} termos mostrados.")
+
